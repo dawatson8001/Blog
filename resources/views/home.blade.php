bea4cf0764f09b42775e897ee09dce1b685cd53e
David Watson <dave.watson8001@gmail.com>
2018-04-24 08:39:07 +0100

    json file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,41 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Blog Home - Start Bootstrap Template</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/blog-home.css" rel="stylesheet">
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
 @section('content')
 
+<div class="container">
+    <div class="row">
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <h1 class="page-header">
+            </h1>
 
+            @foreach($post as $posts)
+                <!-- First Blog Post -->
+                <h2>{{$posts->title}}</a></h2>
+                <p>{{$posts->body}}</p>
+                <hr>
+    @endforeach
+
+</body>
+
+</html>
 
 @endsection
