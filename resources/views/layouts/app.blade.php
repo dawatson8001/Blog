933392a1b76a9fc6ee173b6ae8cc9bf313c19ced
David Watson <dave.watson8001@gmail.com>
2018-04-24 08:59:14 +0100

    database update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
                     <!-- Authentication Links -->
-                    @guest
+                    {{--@guest--}}
                         <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                         <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                     @else
@@ -64,7 +64,7 @@
                                 </form>
                             </div>
                         </li>
-                    @endguest
+                    {{--@endguest--}}
                 </ul>
             </div>
         </div>
