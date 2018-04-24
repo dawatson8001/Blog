7a20068cbf97277a40c3de1715cac9673619c81e
David Watson <dave.watson8001@gmail.com>
2018-04-24 02:51:49 +0100

    updated templates and tidied up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,8 +71,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="/auth/login.blade.php">Login</a>
-                        <a href="/auth/register.blade.php">Register</a>
+                        <a href="{{ url('/login.blade.php') }}">Login</a>
+                        <a href="{{ url('/register.blade.php') }}">Register</a>
                     @endauth
                 </div>
             @endif
