0e851e38c44085f5adc8628dfb2da1afddf1741a
David Watson <dave.watson8001@gmail.com>
2018-04-24 23:41:30 +0100

    update welcome page

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,9 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
+    .css('resources/assets/css/app.css', 'public.css')
 
-.styles([
+mix.styles([
 
     'resources/assets/css/libs/blog-post.css',
     'resources/assets/css/libs/bootstrap.css',
@@ -22,8 +23,9 @@ mix.js('resources/assets/js/app.js', 'public/js')
     'resources/assets/css/libs/metisMenu.css',
     'resources/assets/css/libs/sb-admin-2.css'
 
-], './public/css/libs.css')
-    .scripts([
+], './public/css/libs.css');
+
+mix.scripts([
 
         'resources/assets/js/libs/bootstrap.js',
         'resources/assets/js/libs/jquery.js',
