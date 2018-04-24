0f3bee8627dcc30044509091809bc916b3118016
David Watson <dave.watson8001@gmail.com>
2018-04-24 06:21:12 +0100

    json file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ route('admin.posts.index') }}">BLOG</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
