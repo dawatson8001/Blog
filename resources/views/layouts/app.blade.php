6e15f0f509afab7ed3883dadce45871ca143641e
David Watson <dave.watson8001@gmail.com>
2018-04-24 21:57:18 +0100

    update welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,9 @@
 
     <title>Admin</title>
 
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    <link href="{{asset('css/libs/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/libs/styles.css')}}" rel="stylesheet">
-    <link href="{{asset('css/libs/sb-admin-2.css')}}" rel="stylesheet">
-    <link href="{{asset('css/libs/metisMenu.css')}}" rel="stylesheet">
-    <link href="{{asset('css/libs/blog-post.css')}}" rel="stylesheet">
-    <link href="{{asset('css/libs/font-awesome.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
