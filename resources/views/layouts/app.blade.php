75afb8204f9d76c091429a6a3117c06f5977dc30
David Watson <dave.watson8001@gmail.com>
2018-04-25 05:38:01 +0100

    update welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <ul class="nav navbar-nav">
             @if(@Auth::guest())
                 <a href="{{ secure_url('/login') }}">Login</a>
-                <a href="{{ url('/register') }}">Register</a>
+                <a href="{{ secure_url('/register') }}">Register</a>
             @else
                 <td><a href="{{route('/')}}">Home</a></td>
                 <td><a href="{{route('admin.users.index')}}">View Users</a></td>
