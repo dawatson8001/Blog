beffae7d1f8a57c6a7cd89f2cca9251d617be7eb
David Watson <dave.watson8001@gmail.com>
2018-04-25 05:22:00 +0100

    update welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,11 +51,11 @@
     </div>
 </div>
 
-<script src="{{asset('js/app.js')}}"></script>
-<script src="{{asset('js/libs/metisMenu.js')}}"></script>
-<script src="{{asset('js/libs/jquery.js')}}"></script>
-<script src="{{asset('js/libs/scripts.js')}}"></script>
-<script src="{{asset('js/libs/sb-admin-2.js')}}"></script>
+<script src="{{secure_asset('js/app.js')}}"></script>
+<script src="{{secure_asset('js/libs/metisMenu.js')}}"></script>
+<script src="{{secure_asset('js/libs/jquery.js')}}"></script>
+<script src="{{secure_asset('js/libs/scripts.js')}}"></script>
+<script src="{{secure_asset('js/libs/sb-admin-2.js')}}"></script>
 </body>
 
 </html>
